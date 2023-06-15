1f4ca24dc3165d350999548efb2cd66022f36188
Ahnjaewon <vutus5@naver.com>
2023-06-15 15:35:44 +0900

    login update

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,11 +1,14 @@
 package dao;
 
 import config.JdbcConnection;
+import dto.LoginDto;
 import dto.UserDto;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDao {
     public void insert(UserDto user) {
@@ -18,6 +21,38 @@ public class UserDao {
             pst.setString(2, user.getPassword());
             pst.setString(3, user.getName());
             pst.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean login(LoginDto loginDto) {
+        List<UserDto> users = new ArrayList<>();
+        Connection conn = new JdbcConnection().getJdbc();
+        String sql = "select id,username,name,create_at " +
+                     "from users " +
+                     "where username = ? and password = ?";
+        try {
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, loginDto.getUsername());
+            pst.setString(2, loginDto.getPassword());
+            ResultSet resultSet = pst.executeQuery();
+            while (resultSet.next()) {
+                users.add(makeUser(resultSet));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return users.size() != 0;
+    }
+
+    private UserDto makeUser(ResultSet resultSet) {
+        try {
+            int id1 = resultSet.getInt("id");
+            String username = resultSet.getString("username");
+            String name = resultSet.getString("name");
+            String createdAt = resultSet.getString("create_at");
+            return new UserDto(id1,username,name,createdAt);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,4 +2,6 @@
 <body>
 <h2>Hello World!</h2>
 </body>
+    <a href="login">login</a>
+    <a href="user">signup</a>
 </html>
